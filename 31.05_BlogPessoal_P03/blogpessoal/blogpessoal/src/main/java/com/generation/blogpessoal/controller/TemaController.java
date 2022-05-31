39dbe838c8a6c1b019426bf6304f807faa11437f
unknown <andreiflancanova@hotmail.com>
2022-05-31 19:29:18 -0300

    Upload 31.05 - Blog Pessoal - Parte 03

--- 31.05_BlogPessoal_P03/blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ 31.05_BlogPessoal_P03/blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -2,6 +2,8 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -76,12 +78,12 @@ public class TemaController {
 		}
 		
 		@PostMapping
-		public ResponseEntity<Tema> adicionaTema(@RequestBody Tema tema){
+		public ResponseEntity<Tema> adicionaTema(@Valid @RequestBody Tema tema){
 			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 		}
 		
 		@PutMapping
-		public ResponseEntity<Tema> atualizaTema(@RequestBody Tema tema){
+		public ResponseEntity<Tema> atualizaTema(@Valid @RequestBody Tema tema){
 			return ResponseEntity.ok(repository.save(tema));
 		}
 		
--- 31.05_BlogPessoal_P03/blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ 31.05_BlogPessoal_P03/blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -3,9 +3,11 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Tema;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema,Long>{
 	/*
 	O Jpa converte através do FindAll tudo que está no Java 
